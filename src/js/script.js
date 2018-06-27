0d2b898b72aacc16637ed3c1c2d9027666f43196
MateuszPospiech <pospiechmateusz@gmail.com>
2018-06-27 14:54:13 +0200

    Mobile menu is working

--- src/js/script.js
+++ src/js/script.js
@@ -4,3 +4,20 @@ var person = {
     age: '26'
 };
 console.log(person.name);
+
+/***NAV Mobile***/
+const navButton = document.getElementById('icon-nav');
+const mainNav = document.getElementById('header__center');
+
+navButton.addEventListener('click', addActive);
+
+function addActive(){
+    
+    console.log('asd');
+    mainNav.classList.toggle('active');
+    navButton.classList.toggle('icon-nav-Active');
+
+    
+}
+
+/***END NAV Mobile***/
