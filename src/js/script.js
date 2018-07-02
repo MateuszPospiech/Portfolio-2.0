ad5fd0f181916602ef40b4e78128674e478cada0
MateuszPospiech <pospiechmateusz@gmail.com>
2018-07-02 13:21:41 +0200

    I added 2 more project and rename description on few other

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,3 @@
-var person = {
-    name: 'Mateo',
-    subname: 'Pośpiech',
-    age: '26'
-};
-console.log(person.name);
-
 /***NAV Mobile***/
 const navButton = document.getElementById('icon-nav');
 const mainNav = document.getElementById('header__center');
@@ -13,7 +6,6 @@ navButton.addEventListener('click', addActive);
 
 function addActive(){
     
-    console.log('asd');
     mainNav.classList.toggle('active');
     navButton.classList.toggle('icon-nav-Active');
 }
